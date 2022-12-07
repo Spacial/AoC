20a4ce68a11041461ee3689d38be89523c174ad3
Spacial <spacial@gmail.com>
2022-12-07 17:49:06 -0300

    day 7 started

--- AoC2022/day07/day7.py
+++ AoC2022/day07/day7.py
@@ -5,7 +5,48 @@
 import sys
 
 
+def getRevPath(path):
+    new = []
+    if len(path) == 1:
+        return path[0]
+    else:
+        return [path[:1], [getRevPath(path[1:])]]
+
+
 def part1(data):
+    lfs = []
+    curdir = ''
+    path =  []
+    for i in data:
+        if i[0] == '$':
+            #print('comando:', i)
+            com = i.split(' ')[1]
+            if com == 'cd':
+                args = i.split(' ')[2]
+                if args == '..':
+                    #print('>> path b:', path)
+                    
+                    print(getRevPath(path))
+                    path.pop()
+                    #print('>> path a>', path)
+                else:
+                    curdir = args
+                    path.append(args)
+                print(*path, ":$", com, "args:", args)
+            elif com == 'ls':
+                args = ''
+                print(*path, ":$", com)
+        else:
+            size, file = i.split(' ')
+            espaco = [" " for x in path]
+            if size[0] != 'd':
+                #print('} size:', int(size), 'file:', file)
+                print(*espaco,' |--- ', file, ':', int(size))
+
+            else:
+                #print('} dir:', file)          
+                print(*espaco,' |--- ', file)
+
     return
 
 
